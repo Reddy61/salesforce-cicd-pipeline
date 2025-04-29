2e36ec867c2dc60a4d06f048f37cf5e97eccc00a
Jayadeep Kumar <94867800+Reddy61@users.noreply.github.com>
2025-04-28 19:13:30 -0500

    Verify pre-checks

--- force-app/main/default/lwc/helloLwc/helloLwc.js
+++ force-app/main/default/lwc/helloLwc/helloLwc.js
@@ -2,7 +2,6 @@ import { LightningElement } from 'lwc';
 
 export default class HelloLwc extends LightningElement {
     handleClick() {
-        // PMD might catch this unused variable
         const greeting = 'Hello World';
         alert('You clicked the button!');
     }
